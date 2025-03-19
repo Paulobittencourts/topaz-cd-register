c03a3b8fbb1d1e556965ea2b727a3b5ebb6c9cba
Paulo Bitencourth <paulobittencourt94@gmail.com>
2025-03-19 19:57:06 -0300

    Add Flyway migration scripts and database configurations

---
+++ src/main/resources/db/migration/V1__create_tables_cds.sql
@@ -0,0 +1,14 @@
+CREATE TABLE cd_collection(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULl,
+    artist VARCHAR(255) NOT NULL,
+    genre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE cd_tracks(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    cd_id INTEGER NOT NULL,
+    track_name VARCHAR(255) NOT NULL,
+    duration TIME NOT NULL,
+    FOREIGN KEY (cd_id) REFERENCES cd_collection(id) ON DELETE CASCADE
+);
