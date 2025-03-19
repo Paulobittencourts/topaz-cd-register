af8fbe761aa3a534db8404abd86313dea5a192e2
Paulo Bitencourth <paulobittencourt94@gmail.com>
2025-03-19 18:03:40 -0300

    Init project

---
+++ src/main/java/com/br/paulohbs/registraion_cd/RegistraionCdApplication.java
@@ -0,0 +1,13 @@
+package com.br.paulohbs.registraion_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistraionCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistraionCdApplication.class, args);
+	}
+
+}
